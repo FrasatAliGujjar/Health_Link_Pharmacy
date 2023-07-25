e797e035fda22bdb6bfcca808593851ad2174f5e
Frasat ALi Gujjar <127954439+FrasatAliGujjar@users.noreply.github.com>
2023-07-24 17:52:49 -0700

    v1
    v1

---
+++ script.js
@@ -0,0 +1,43 @@
+// ================================================================
+function wa1() {
+
+    let url = "http://wa.me/923349770718?text="
+
+        + "*Asslam-o-Alaikum !*" + " " + "%0a"
+        + " " + "%0a"
+
+
+    window.open(url, "_blank");
+
+}
+function wa2() {
+
+    let url = "http://wa.me/923270209991?text="
+
+        + "*Asslam-o-Alaikum !*" + " " + "%0a"
+        + " " + "%0a"
+
+
+    window.open(url, "_blank");
+
+}
+function order() {
+
+    let url = "http://wa.me/923349770718?text="
+
+        + "*Asslam-o-Alaikum !*" + " " + "%0a"
+        + " " + "%0a"
+        + "I want to order some medicines" + "%0a"
+        + "__________________________________" + "%0a"
+        + " " + "%0a"
+        + "*Name :*" + " " + document.getElementById('name').value + "%0a"
+        + "*Age :*" + " " + document.getElementById('age').value + "%0a"
+        + "*Medicine Name :*" + " " + document.getElementById('mdn').value + "%0a"
+        + "*Numbers of pills*" + " " + document.getElementById('no').value + "%0a"
+        + " " + "%0a"
+        + "Thanks :)" + "%0a"
+
+
+    window.open(url, "_blank");
+
+}
