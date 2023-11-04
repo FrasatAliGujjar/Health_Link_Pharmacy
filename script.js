1dba2297a07574afbb440fcadffa28cc6c4e847d
Frasat ALi Gujjar <127954439+FrasatAliGujjar@users.noreply.github.com>
2023-11-03 20:27:54 -0700

    vv
    vv

--- script.js
+++ script.js
@@ -1,42 +1,31 @@
-// ================================================================
-function wa1() {
+// =================== Appointment Request Send ===================== 
 
-    let url = "http://wa.me/923349770718?text="
+// let namee = document.getElementById('name').value;
+// let gender = document.getElementById('gender').value;
+// let problem = document.getElementById('problem').value;
+// let wa = document.getElementById('wa').value;
 
-        + "*Asslam-o-Alaikum !*" + " " + "%0a"
-        + " " + "%0a"
+function whatsapp() {
 
+    let url = "http://wa.me/923000174141?text="
 
-    window.open(url, "_blank");
-
-}
-function wa2() {
-
-    let url = "http://wa.me/923270209991?text="
-
-        + "*Asslam-o-Alaikum !*" + " " + "%0a"
-        + " " + "%0a"
-
-
-    window.open(url, "_blank");
-
-}
-function order() {
-
-    let url = "http://wa.me/923349770718?text="
-
-        + "*Asslam-o-Alaikum !*" + " " + "%0a"
-        + " " + "%0a"
-        + "I want to order some medicines" + "%0a"
-        + "__________________________________" + "%0a"
-        + " " + "%0a"
         + "*Name :*" + " " + document.getElementById('name').value + "%0a"
-        + "*Age :*" + " " + document.getElementById('age').value + "%0a"
-        + "*Medicine Name :*" + " " + document.getElementById('mdn').value + "%0a"
-        + "*Numbers of pills*" + " " + document.getElementById('no').value + "%0a"
-        + " " + "%0a"
-        + "Thanks :)" + "%0a"
+        + "*Gender :*" + " " + document.getElementById('gender').value + "%0a"
+        + "*Problem :*" + " " + document.getElementById('problem').value + "%0a"
+        + "*Whatsapp :*" + " " + document.getElementById('wa').value + "%0a"
 
+    window.open(url, "_blank");
+
+}
+
+function whatsapp2() {
+
+    let url = "http://wa.me/923121718036?text="
+
+        + "*Name :*" + " " + document.getElementById('name').value + "%0a"
+        + "*Gender :*" + " " + document.getElementById('gender').value + "%0a"
+        + "*Problem :*" + " " + document.getElementById('problem').value + "%0a"
+        + "*Whatsapp :*" + " " + document.getElementById('wa').value + "%0a"
 
     window.open(url, "_blank");
 
